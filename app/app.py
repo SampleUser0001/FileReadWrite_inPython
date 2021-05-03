93865bc3c5d717747cb39ed3eaa7d0ace8172826
Satoru Tanaka <ittimfn@gmail.com>
2021-05-03 04:39:36 +0000

    ディレクトリから対象ファイルを検索するように修正

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta, timezone
 import locale
+import glob
+import os
 
-INPUT_PATH = './files/input.txt'
+INPUT_PATH = './files/**'
+
 
 DATE_FORMAT='%Y%m%d_%H%M%S'
 
@@ -10,9 +13,13 @@ JST = timezone(timedelta(hours=+9), 'JST')
 now = datetime.now(JST).strftime(DATE_FORMAT)
 OUTPUT_PATH = './files/output_' + now + '.txt'
 
+
+files = [p for p in glob.glob(INPUT_PATH , recursive=True) if os.path.isfile(p)]
+print(files)
+
 lines=[]
 
-with open(INPUT_PATH) as f:
+with open(files[0]) as f:
     for line in f.read().splitlines():
         print(line)
         lines.append(line)
